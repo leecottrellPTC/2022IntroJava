f28038762acd1881b7564ff2d2ba6e5be4a42842
Lee Cottrell <44240965+leecottrellPTC@users.noreply.github.com>
2022-10-14 11:47:31 -0400

    Week 1 Lecture 1
    Demonstrates println and JOptionPane.showMessageDialog()

---
+++ Week 1/Lecture1/src/App.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Lee Cottrell " + 42);
+        System.out.println("Laurie");
+
+        JOptionPane.showMessageDialog(null, 
+        "Hi there class",
+         "THis is the title", JOptionPane.ERROR_MESSAGE);
+    }
+}
