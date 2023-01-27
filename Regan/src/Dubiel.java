00744b47a462a98cb4d7ce39bee8d32f54d38067
Lee Cottrell <44240965+leecottrellPTC@users.noreply.github.com>
2023-01-27 11:35:26 -0500

    created regan
    fsddsfa

---
+++ Regan/src/Dubiel.java
@@ -0,0 +1,8 @@
+public class Dubiel {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Regan!");
+        int age = 100;
+        double salary = 25646.78;
+
+    }
+}
